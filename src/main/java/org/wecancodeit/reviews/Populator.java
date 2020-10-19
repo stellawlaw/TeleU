229ef46a13d21f6b760787503363ef25254600f4
stella.w.law@gmail.com <stella.w.law@gmail.com>
2020-10-19 15:11:09 -0400

    review page connected!

--- src/main/java/org/wecancodeit/reviews/Populator.java
+++ src/main/java/org/wecancodeit/reviews/Populator.java
@@ -26,17 +26,17 @@ public class Populator implements CommandLineRunner {
 
         TvShowReviews americanHorrorStory = new TvShowReviews(horror, "American Horror Story", "January", "nine", "It's bs", "Three Stars", "/Images/American_Horror_Story.jpg");
 
-        TvShowReviews strangerThingsSeason2 = new TvShowReviews(horror, "Stranger Things season 2", "November", "two", "It's bs", "Three Stars", "/Images/Stranger_Things_season_2.jpg");
+        TvShowReviews strangerThingsSeason2 = new TvShowReviews(horror, "Stranger Things", "November", "two", "It's bs", "Three Stars", "/Images/Stranger_Things_season_2.jpg");
 
-        TvShowReviews theOffice = new TvShowReviews(comedy, "the office", "march", "five", "it's alright", "one star", "/Images/The-Office.jpg");
+        TvShowReviews theOffice = new TvShowReviews(comedy, "The Office", "march", "five", "it's alright", "one star", "/Images/The-Office.jpg");
 
-        TvShowReviews pAndR = new TvShowReviews(comedy, "P&R", "march", "six", "it's okay", "two star", "/Images/P&R.jpg");
+        TvShowReviews pAndR = new TvShowReviews(comedy, "Parks and Recreation", "march", "six", "it's okay", "two star", "/Images/P&R.jpg");
 
         tvShowReviewsStorage.addTvShowReview(americanHorrorStory);
         tvShowReviewsStorage.addTvShowReview(strangerThingsSeason2);
         tvShowReviewsStorage.addTvShowReview(theOffice);
         tvShowReviewsStorage.addTvShowReview(pAndR);
-        
+
 
     }
 }
