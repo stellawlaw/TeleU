99b788852fcd1b725ce888947d4c155e1d597aac
David Granados <davidcolumbus2@gmail.com>
2020-10-21 10:58:05 -0400

    10 am work

--- src/main/java/org/wecancodeit/reviews/Contoller/HashtagsController.java
+++ src/main/java/org/wecancodeit/reviews/Contoller/HashtagsController.java
@@ -5,11 +5,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.wecancodeit.reviews.Storage.HashtagsStorage;
+import org.wecancodeit.reviews.Storage.TvShowReviewsStorage;
 
 @Controller
 public class HashtagsController {
-
+    private TvShowReviewsStorage tvShowReviewsStorage;
     private HashtagsStorage hashtagsStorage;
+//    private HashtagHomeController hashtagHomeController;
 
     public HashtagsController(HashtagsStorage hashtagsStorage){
         this.hashtagsStorage = hashtagsStorage;
@@ -20,9 +22,10 @@ public class HashtagsController {
         return "HashtagTemplate";
     }
 
-//    @RequestMapping("{reviews}hashtags/{id}")
-//    public String showAllHashtags(Model model, @PathVariable long id, @PathVariable String reviews){
-//        model.addAttribute("allhashtags", hashtagsStorage.retrieveAllHashtags());
-//        return "AllHashtagTemplate";
-//    }
+
+    @RequestMapping("{reviews}hashtags/{id}")
+    public String showAllReviews(Model model, @PathVariable long id, @PathVariable String reviews){
+        model.addAttribute("allReviews", tvShowReviewsStorage.retrieveOneReviewById(id));
+        return "HashtagTemplate";
+    }
 }
--- src/main/java/org/wecancodeit/reviews/Contoller/TvShowReviewsController.java
+++ src/main/java/org/wecancodeit/reviews/Contoller/TvShowReviewsController.java
@@ -2,22 +2,33 @@ package org.wecancodeit.reviews.Contoller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.wecancodeit.reviews.Storage.HashtagsStorage;
 import org.wecancodeit.reviews.Storage.TvShowReviewsStorage;
+import org.wecancodeit.reviews.models.Hashtags;
 
 @Controller
 public class TvShowReviewsController {
 
     private TvShowReviewsStorage tvShowReviewsStorage;
+//    private HashtagsStorage hashtagsStorage;
 
     public TvShowReviewsController(TvShowReviewsStorage tvShowReviewsStorage) {
         this.tvShowReviewsStorage = tvShowReviewsStorage;
+
     }
 
-    @RequestMapping("{genre}/reviews/{id}")
+    @GetMapping ("{genre}/reviews/{id}")
     public String showOneReview(Model model, @PathVariable long id, @PathVariable String genre) {
         model.addAttribute("TvShowReviews", tvShowReviewsStorage.retrieveOneReviewById(id));
         return "TvShowsReviews-template";
     }
+//    @PostMapping ("/hashtags/")
+//    public String addHashtag(String hashtagName){
+//        Hashtags hashtagsToAdd = new Hashtags(hashtagName);
+//        HashtagsStorage.add
+//    }
+
 }
